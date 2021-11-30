0151652e5218186f35cb1fd87f9bdf88c9c82979
Rame Madah <lawkira10@gmail.com>
2021-11-30 22:07:40 +0100

    Hello World

---
+++ src/views/Lesson.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="lesson">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Lesson'
+}
+</script>
+
+<style scoped>
+.lesson{
+  max-width: 500px;
+  margin: auto;
+}
+</style>
